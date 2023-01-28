5368a44dfb1fccd6bc3174dea7b64bfeb60e98b2
Mildred Makori <makorimildred@gmail.com>
2023-01-28 08:24:49 +0400

    changed the theme color of google login button
    changed the google theme color from blue to white

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv/config");
+require("dotenv/config"); // configure reading from .env
 const cors = require("cors");
 const { OAuth2Client } = require("google-auth-library");
 const jwt = require("jsonwebtoken");
--- user_dashboard/src/screens/Landing.jsx
+++ user_dashboard/src/screens/Landing.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Login } from "."
-import { Signup } from ".";
+import { Login, Signup } from ".";
 
 const Landing = () => {
   return (
